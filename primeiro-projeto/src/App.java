21403f072be3ec0002cf337c5220e1ad20c779cd
Robson Alex <carloscostammfata@gmail.com>
2024-08-03 00:03:42 -0300

    atualizando

--- primeiro-projeto/src/App.java
+++ primeiro-projeto/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("seja bem vindo!");
+        System.out.println("seja bem vindo! aqui");
     }
 }
