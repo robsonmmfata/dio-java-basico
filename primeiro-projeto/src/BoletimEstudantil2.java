d66a768d2e9c2417b7b201932a654278601e7e51
Robson alex <100169808+robsonmmfata@users.noreply.github.com>
2024-08-04 15:20:15 -0300

    estudos basicos do conceito

---
+++ primeiro-projeto/src/BoletimEstudantil2.java
@@ -0,0 +1,12 @@
+//agora com indentação
+public class BoletimEstudantil2 {
+    public static void main(String[] args) {
+        int mediaFinal = 8;
+        if (mediaFinal < 6)
+        System.out.println("Você está reprovado");
+        else if(mediaFinal == 6)
+        System.out.println("Você está de recuperação");
+        else
+        System.out.println("Você está aprovado");
+    }
+}
